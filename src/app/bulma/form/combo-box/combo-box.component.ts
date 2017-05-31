637662ac2ac9a4e478d12787267695c35f32eb6b
tobias.pickel <tobias.pickel@rsu-reifen.de>
2017-05-31 17:32:47 +0200

    added key events to comboBox

--- src/app/bulma/form/combo-box/combo-box.component.ts
+++ src/app/bulma/form/combo-box/combo-box.component.ts
@@ -1,11 +1,11 @@
-import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, HostListener, Input, OnChanges, OnInit, Output, ViewChild} from '@angular/core';
 import {StringFilterService} from '../shared/string-filter.service';
 import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'bu-combo-box',
   templateUrl: './combo-box.component.html',
-  styleUrls: ['./combo-box.component.scss'],
+  styleUrls: ['./combo-box.component.scss']
 })
 export class ComboBoxComponent implements OnInit, OnChanges {
 
@@ -13,12 +13,33 @@ export class ComboBoxComponent implements OnInit, OnChanges {
   @Input() query: string;
   @Input() list: any[];
   @Input() display: string;
+  @Input() selected: any[];
+  @Input() listLimit = 10;
 
   @Output() onSelect = new EventEmitter();
+
+  @ViewChild('dropDown') dropDown;
 
   filteredList: any[];
   queryControl: FormControl;
 
+
+  @HostListener('keydown', ['$event.code']) onKeydown(code) {
+    switch (code) {
+      case 'ArrowUp':
+        this.selectPrev();
+        break;
+      case 'ArrowDown':
+        this.selectNext();
+        break;
+      case 'Escape':
+        this.resetInput();
+        break;
+      case 'Enter':
+        this.select(this.selected);
+        break;
+    }
+  }
 
   constructor(private filterService: StringFilterService) {
     this.queryControl = new FormControl();
@@ -26,7 +47,7 @@ export class ComboBoxComponent implements OnInit, OnChanges {
   }
 
   ngOnInit() {
-
+    console.log(this.dropDown);
   }
 
   ngOnChanges(changes) {
@@ -35,14 +56,67 @@ export class ComboBoxComponent implements OnInit, OnChanges {
     }
   }
 
+  selectNext() {
+    this.dropDown.triggerOpen();
 
-  filter(query: string) {
-    this.filteredList = this.filterService.filter(this.list, this.display, query);
+    const lastIndex = Math.min(this.listLimit, this.list.length) - 1;
+    let index = this.list.indexOf(this.selected);
+    index = index >= this.listLimit ? lastIndex : index;
+
+    index += 1;
+
+    index = index >= this.listLimit ? lastIndex : index;
+
+    this.selectByIndex(index);
   }
 
-  select(item): void {
-    if (item) {
-      this.onSelect.emit({selected: item});
+  selectPrev() {
+    let index = this.list.indexOf(this.selected);
+    index = index >= this.listLimit ? 0 : index;
+
+    index -= 1;
+
+    index = index >= this.listLimit ? 0 : index;
+
+    this.selectByIndex(index);
+  }
+
+  selectByIndex(index: number): void {
+    const nextItem = this.list[index];
+
+    if (nextItem) {
+      this.preselect(nextItem);
     }
+  }
+
+  resetInput() {
+    this.queryControl.reset();
+  }
+
+  filter(query: string) {
+    const filteredList = this.filterService
+      .filter(this.list, this.display, query)
+      .slice(0, this.listLimit);
+
+    if (filteredList.length) {
+      this.dropDown.triggerOpen();
+    }
+
+    this.filteredList = filteredList;
+  }
+
+  preselect(item): void {
+    if (item) {
+      this.selected = item;
+    }
+  }
+
+  select(item) {
+    if (item && item[this.display]) {
+      this.onSelect.emit({selected: item});
+      this.queryControl.setValue(item[this.display]);
+      this.dropDown.triggerClose();
+    }
+
   }
 }
