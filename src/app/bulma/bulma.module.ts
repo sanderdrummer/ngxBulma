03848debdb2ab542bbda50fe97bc43db2d4c02c1
tobias.pickel <tobias.pickel@rsu-reifen.de>
2017-05-31 17:01:06 +0200

    refactored namings

--- src/app/bulma/bulma.module.ts
+++ src/app/bulma/bulma.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {BuModalModule} from './bu-modal/bu-modal.module';
-import {FieldComponent} from './bu-form/field/field.component';
-import {ComboBoxComponent} from './bu-form/combo-box/combo-box.component';
-import {BuFormModule} from './bu-form/bu-form.module';
-import {StringFilterService} from './bu-form/shared/string-filter.service';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {BuModalModule} from './modal/bu-modal.module';
+import {FieldComponent} from './form/field/field.component';
+import {ComboBoxComponent} from './form/combo-box/combo-box.component';
+import {BuFormModule} from './form/bu-form.module';
+import {StringFilterService} from './form/shared/string-filter.service';
 import {DropDownComponent} from './drop-down/drop-down.component';
 import {BuDropDownModule} from './drop-down/bu-drop-down.module';
 
@@ -19,4 +19,5 @@ import {BuDropDownModule} from './drop-down/bu-drop-down.module';
   exports: [BuModalModule, ComboBoxComponent, FieldComponent, DropDownComponent],
   providers: [StringFilterService]
 })
-export class BulmaModule { }
+export class BulmaModule {
+}
--- src/app/bulma/form/bu-form.module.ts
+++ src/app/bulma/form/bu-form.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ComboBoxComponent } from './combo-box/combo-box.component';
-import { FieldComponent } from './field/field.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ComboBoxComponent} from './combo-box/combo-box.component';
+import {FieldComponent} from './field/field.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {BuDropDownModule} from '../drop-down/bu-drop-down.module';
 
@@ -15,4 +15,5 @@ import {BuDropDownModule} from '../drop-down/bu-drop-down.module';
   exports: [ComboBoxComponent, FieldComponent]
 
 })
-export class BuFormModule { }
+export class BuFormModule {
+}
